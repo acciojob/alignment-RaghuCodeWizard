413bb070d314161d892687e1015139dd0a649f4f
Raghavendra M <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-08-31 16:02:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-//your JS code here. If required.
-describe("HTML Assignment #3", () => {
-    const baseUrl = "http://localhost:3000"; // Replace with your base URL
-    
-    it("should assert colspan attribute", () => {
-        cy.visit(baseUrl);
-        cy.get('.center[colspan="6"]').should('have.length', 2);
-    });
 
-    it("should assert padding and border styles", () => {
-        cy.visit(baseUrl);
-        cy.get('td')
-            .should('have.css', 'padding', '1px')
-            .and('have.css', 'border', '1px solid rgb(0, 0, 0)');
-    });
-});
